803fbc8af6ff627e65d94650edb48ff4c2ea8aad
Jeffrey <jeffreyqdd@gmail.com>
2024-10-27 23:33:57 -0500

    format shell file and changed to use python3 instead of python3.10 to reflect system

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 if [[ "$(basename -- "$0")" == "setup.sh" ]]; then
-    >&2 echo "Don't run $0, source it"
+    echo >&2 "Don't run $0, source it"
     exit 1
 fi
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 if [[ $(which python3) == "${SCRIPT_DIR}/.env/bin/python3" ]]; then
     echo "already in virtual env"
@@ -13,12 +13,12 @@ fi
 
 if [[ ! -d ".env/" ]]; then
     echo "installing python environment"
-    python3.10 -m venv .env
-    source .env/bin/activate 
+    python3 -m venv .env
+    source .env/bin/activate
     pip3 install -r requirements.txt
 else
     echo "activating python environment"
-    source .env/bin/activate 
+    source .env/bin/activate
 fi
 
 echo "setting python path to $SCRIPT_DIR"
