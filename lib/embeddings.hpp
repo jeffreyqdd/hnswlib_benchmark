cb67cfa2d921ea16bc4dc87fd654ec9e698392d1
Jeffrey <jeffreyqdd@gmail.com>
2024-10-29 19:24:23 -0500

    read and validate on gist360 dataset

--- lib/embeddings.hpp
+++ lib/embeddings.hpp
@@ -1,80 +1,79 @@
 /* Meant to be a helper file to generate embeddings and the ground truth */
 #pragma once
 
-#include <cassert>
-#include <cstdint>
+#include <filesystem>
+#include <fstream>
 #include <memory>
-#include <queue>
-#include <random>
 #include <unordered_set>
-#include <vector>
 
-#include <faiss/IndexFlat.h>
-
-/// @brief Embedding struct container vector embedding data as well as associated metadata
+template <typename T>
 struct Embedding {
-	const std::unique_ptr<float[]> data;
-	const size_t nb;
-	const size_t dim;
+	const std::unique_ptr<T[]> data;
+	const int dim;
+	const int nb;
 };
 
-/// @brief generate vector embeddings sampled from a seeded uniform distribution in contiguous memory
-/// @param nb number of vectors
-/// @param dim dimension of each vector
-/// @return flattened pointer of the vectors
-Embedding generate_test_vectors(size_t nb, size_t dim, uint_fast32_t seed = 47) {
-	std::mt19937 rng;
-	rng.seed(47);
+/// @brief read gist dataset
+/// @param gist_src path to gist fvecs file
+/// @param dim dimension of dataset is stored in dim
+/// @param nb number of vectors is stored in nb
+/// @return
+template <typename T>
+Embedding<T> load_gist_960(const std::filesystem::path& gist_src) {
+	std::ifstream fin(gist_src, std::ios::binary);
 
-	std::uniform_real_distribution<> uniform_distribution;
+	if(!fin) {
+		throw std::runtime_error(std::format("could not open filename {}", gist_src.string()));
+	}
+	int dim;
+	int nb;
+	fin.read(reinterpret_cast<char*>(&dim), sizeof(T));
 
-	std::unique_ptr<float[]> ret = std::make_unique<float[]>(nb * dim);
-	for(size_t i = 0; i < dim * nb; i++) {
-		ret[i] = uniform_distribution(rng);
+	if(!fin) {
+		throw std::runtime_error("could not read dimensions");
 	}
 
-	return { std::move(ret), nb, dim };
-}
+	const size_t file_size = std::filesystem::file_size(gist_src);
+	nb = file_size / (dim * sizeof(T) + sizeof(int));
 
-/// @brief calculate the ground truth using the faiss library which should be reasonably fast
-/// @param embedding vector embeddings
-/// @param query query vector that has the same dimension as that of the vector embedding
-/// @param top_k top k nearest neighbors
-/// @return array of distances and indicies
-std::pair<std::unique_ptr<float[]>, std::unique_ptr<faiss::idx_t[]>>
-get_top_k_euclidean(const Embedding& embedding, const float* query, size_t top_k) {
-	assert(top_k >= embedding.nb);
+	std::unique_ptr<T[]> data = std::make_unique<T[]>(nb * dim);
+	fin.seekg(0, std::ios::beg);
+	for(size_t i = 0; i < static_cast<size_t>(nb); i++) {
+		// read dim
+		int tmp_dim;
+		fin.read(reinterpret_cast<char*>(&tmp_dim), sizeof(int));
+		assert(tmp_dim == dim);
 
-	// use faiss to get ground truth
-	faiss::IndexFlatL2 index(embedding.dim);
-	index.add(embedding.nb, embedding.data.get());
-
-	std::unique_ptr<float[]> distances = std::make_unique<float[]>(top_k);
-	std::unique_ptr<faiss::idx_t[]> indices = std::make_unique<faiss::idx_t[]>(top_k);
-
-	index.search(1, query, top_k, distances.get(), indices.get());
-	return std::make_pair(std::move(distances), std::move(indices));
-}
-
-double calculate_recall(const Embedding& embedding,
-						const std::vector<size_t> results,
-						const float* query) {
-
-	const size_t top_k = results.size();
-	auto [_, indicies] = get_top_k_euclidean(embedding, query, top_k);
-
-	std::vector<bool> contains(embedding.nb, false);
-	for(size_t i = 0; i < top_k; i++) {
-		contains[indicies[i]] = true;
+		//read vector
+		fin.read(reinterpret_cast<char*>(data.get() + i * dim), tmp_dim * sizeof(T));
 	}
 
-	size_t num_hits = 0;
-	for(const size_t& item : results) {
-		assert(item < embedding.nb);
-		if(contains[item]) {
+	return { std::move(data), dim, nb };
+}
+
+double calculate_recall(const int query_id,
+						const Embedding<int>& ground_truth,
+						std::priority_queue<std::pair<float, hnswlib::labeltype>>& results) {
+
+	const int top_k = ground_truth.nb;
+	const int total_queries = ground_truth.dim;
+	const int* truth_ptr = ground_truth.data.get();
+
+	std::unordered_set<bool> contains;
+	for(int i = 0; i < top_k; i++) {
+		const int* vector_id = truth_ptr + (query_id + total_queries * i);
+		contains.insert(*vector_id);
+	}
+
+	int num_hits = 0;
+	while(!results.empty()) {
+		auto [dist, label] = results.top();
+		results.pop();
+
+		if(contains.contains(label)) {
 			num_hits++;
 		}
 	}
 
-	return static_cast<double>(num_hits) / top_k;
+	return static_cast<double>(num_hits) / static_cast<double>(top_k);
 }
